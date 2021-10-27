7539674afd6e246da67e93b792f823fd68e7c8da
Viorel Soltan <joomjoom2011@gmail.com>
2021-10-27 13:45:06 +0300

    update file

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,4 @@
-{{-- @extends('products.layout') --}}
-{{-- @extends('layouts.app')
-@section('content') --}}
+
 <x-app-layout>
   <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -56,4 +54,3 @@
 </div>
 
 </x-app-layout>
-{{-- @endsection --}}
